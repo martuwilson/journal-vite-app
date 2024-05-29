36eb38d837fb6b656f73c9e69fe3992e5c01f6c7
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-05-28 22:17:38 -0300

    MUI material added and cofig for vite

---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { ThemeProvider } from "@emotion/react"
+import { CssBaseline } from "@mui/material"
+import { purpleTheme } from "./purpleTheme"
+
+
+export const AppTheme = ({ children }) => {
+  return (
+    <ThemeProvider theme={ purpleTheme }>
+        <CssBaseline/>
+        {children}
+    </ThemeProvider>)
+}
