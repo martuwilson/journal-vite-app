123accd437002e1ecd166696ceb45541e519ad5a
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-05-31 23:04:32 -0300

    AuthLayout agregado y reemplazo de secciones en LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,27 +2,12 @@ import {Link as RouterLink} from "react-router-dom";
 
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }} //style extended
-    >
-      <Grid
-        item
-        className="box-shadow"
-        xs={3}
-        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 5 }}>
-          Login
-        </Typography>
+    <AuthLayout title='Login'>
 
         <form>
           <Grid container>
@@ -69,7 +54,6 @@ export const LoginPage = () => {
 
 
         </form>
-      </Grid>
-    </Grid>
+    </AuthLayout>
   );
 };
